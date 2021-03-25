ec866152365a84abaef83ec1476411b625758fde
Soundous <soudous.bb@gmail.com>
2021-03-25 16:37:42 +0100

    Constructeur crée

---
+++ src/Brin.java
@@ -0,0 +1,17 @@
+public class Brin {
+
+    private String sequence;
+    private String appariement;
+
+
+    public Brin(String sequence, String appariement){
+        this.sequence = sequence;
+        this.appariement = appariement;
+    }
+
+    public static void main(String[] args){
+        Brin l = new Brin("AGCUACG", "((-))-()");
+
+    /*creer methode pour verifier sequence va avec appariement*/
+    }
+}
